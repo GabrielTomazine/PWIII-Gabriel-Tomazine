16b8fd6d9fe71936f29112b78480839f2a4b1888
Gabriel Tomazine <gtomazine1@gmail.com>
2025-02-26 10:11:39 -0300

    Arquivo de Exercícios e Pasta Com as Resoluções

---
+++ AulasPHP/aula1.php
@@ -0,0 +1,21 @@
+<?php
+//Exercícios
+
+echo "1-) ";
+
+$val1 = 10;
+$val2 = 10;
+$val3 =  10;
+
+$result1 = $val1 + $val2 + $val3;
+$result = $result1 / 3;
+
+echo "Média: ", $result;
+
+echo "<br><br><br><br>";
+
+echo "2-) ";
+
+$valor1|
+
+?>
