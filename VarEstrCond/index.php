2b0c4619753a21c98b9ffd4cc86b2e3285463278
Gabriel Tomazine <gtomazine1@gmail.com>
2025-04-02 17:57:25 -0300

    Conexão PHP e Database

---
+++ VarEstrCond/index.php
@@ -0,0 +1,25 @@
+<?php
+
+echo "Ex 1";
+echo "<br><br>";
+
+$v1 = 20;
+$v2 = 4;
+$soma = $v1 + $v2;
+$mult = $v1 * $v2;
+$sub = $v1 - $v2;
+$div = $v1 / $v2;
+
+echo "A soma dos valores é: ",$soma,". <br>A multiplicação dos valores é: ",$mult,". <br>O valor da subtração é: ",$sub, ". <br>O valor da divsão é: ",$div;
+
+echo "<br><br><br><br>";
+echo "Ex 2";
+echo "<br><br>";
+
+$distT = 100;
+$cons = 90;
+$TGC = $distT / $cons;
+
+echo "O consumo médio do automóvel é: ", $TGC;
+
+?>
